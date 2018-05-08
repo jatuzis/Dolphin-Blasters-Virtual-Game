e031e0c61ff73ce8c85c9b27120413709f597190
DESKTOP-P1DU2EF\kraem <kraemerfa@t-online.de>
2018-05-08 11:55:27 +0200

    fixed rotation bug
    atleast i guess that i fixed it

--- DolphinBlasters-Virtual-Game/Assets/Scripts/PlayerMovement.cs
+++ DolphinBlasters-Virtual-Game/Assets/Scripts/PlayerMovement.cs
@@ -23,14 +23,17 @@ public class PlayerMovement : MonoBehaviour {
 
     void Move()
     {
+        Debug.Log("Horizontal: " + Input.GetAxis("Horizontal") + "Vertical: " + Input.GetAxis("Vertical"));
+
         Vector3 velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * _movement_speed * Time.deltaTime;
+        Debug.Log(velocity);
         _rb.velocity = velocity;
     }
 
     void Rotate()
     {
         Vector3 velocity = _rb.velocity;
-        if (velocity.magnitude > 0.15f)
+        if (velocity.magnitude > 0.2f)
         {
             transform.rotation = Quaternion.LookRotation(new Vector3(velocity.x, 0, velocity.z));
         }
