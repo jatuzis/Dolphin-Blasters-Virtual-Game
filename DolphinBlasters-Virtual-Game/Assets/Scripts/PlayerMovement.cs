c4818a3003726c6b9f5099c247fed5f34b2e24e6
DESKTOP-P1DU2EF\kraem <kraemerfa@t-online.de>
2018-05-08 11:12:07 +0200

    Added simple movement
    player can move and always looks in move direction

---
+++ DolphinBlasters-Virtual-Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float _movement_speed;
+
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    void Move()
+    {
+        Vector3 velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * _movement_speed * Time.deltaTime;
+        _rb.velocity = velocity;
+    }
+
+    void Rotate()
+    {
+        Vector3 velocity = _rb.velocity;
+        transform.rotation = Quaternion.LookRotation(new Vector3(velocity.x, 0, velocity.z));
+    }
+}
