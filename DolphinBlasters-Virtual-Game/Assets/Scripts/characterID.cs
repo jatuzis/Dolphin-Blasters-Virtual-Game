d00e8dba1e262295033413d7d62da4dc9ce4fe32
LAPTOP-UBUK1R6P\Pascal <pascal-diroll@gmx.de>
2018-05-09 11:54:31 +0200

    character selection basics

---
+++ DolphinBlasters-Virtual-Game/Assets/Scripts/characterID.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterID : MonoBehaviour {
+
+	// Use this for initialization
+	[SerializeField]
+	private string _name;
+
+//	public string Name
+//	{
+//		get { _name; }
+//	}
+}
